59fa2f28b7fbec82a92b3462f2b4cb52d74cc158
bohatel <razvan.bohatel@gmail.com>
2023-04-11 15:38:59 +0300

    illuminate

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -204,6 +204,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/razvan/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-illuminate"] = {
+    loaded = true,
+    path = "/home/razvan/.local/share/nvim/site/pack/packer/start/vim-illuminate",
+    url = "https://github.com/RRethy/vim-illuminate"
   }
 }
 
