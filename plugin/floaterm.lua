038bb1daa57977fa05d679fbd6ee59066d95c11c
bohatel <razvan.bohatel@gmail.com>
2023-04-13 15:44:50 +0300

    update floaterm mappings

--- plugin/floaterm.lua
+++ plugin/floaterm.lua
@@ -1,5 +1,5 @@
-vim.g.floaterm_keymap_toggle = '<F12>'
+vim.g.floaterm_keymap_toggle = '<Leader><F12>'
 vim.g.floaterm_keymap_new = '<Leader>ft'
-vim.g.floaterm_keymap_next = '<F8>'
-vim.g.floaterm_keymap_prev = '<F7>'
+vim.g.floaterm_keymap_next = '<F7>'
+vim.g.floaterm_keymap_prev = '<Leader><F7>'
 
